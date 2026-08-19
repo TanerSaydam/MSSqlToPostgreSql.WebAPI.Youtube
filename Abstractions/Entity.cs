c1f1afcf306728cc07ffec2a35441542e997e9b9
Taner Saydam <taner_and_you@hotmail.com>
2026-08-19 11:33:54 +0300

    I added nullable to update at property

--- Abstractions/Entity.cs
+++ Abstractions/Entity.cs
@@ -8,5 +8,5 @@ public abstract class Entity
     }
     public Guid Id { get; set; }
     public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
 }
